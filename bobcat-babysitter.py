9533790782ddbb4caf8f436a12f35b2ef7ae25dd
Randy C. Will <randall.will@gmail.com>
2021-11-07 20:57:01 -0800

    Miner status monitor and WeMo support

--- bobcat-babysitter.py
+++ bobcat-babysitter.py
@@ -1,19 +1,43 @@
-#!/usr/bin/env python
+#!/usr/bin/python3 -u
 
 import time
+import pywemo
 from bobcat import Bobcat
 
 bobcat = Bobcat('your bobcat ip')
+switch = pywemo.discovery.device_from_description('http://your-wemo-ip:49153/setup.xml')
 
 sleep_mins = 5
 
 print("Entering loop")
-bobcat.last_gap = bobcat.gap
-
-while True:
-    print([getattr(bobcat, attr) for attr in ['timestamp','uptime','status','gap','miner_height','blockchain_height','ota_version','temp0','temp1','errors']])
-    if bobcat.gap > 10 and bobcat.gap > bobcat.last_gap:
-        bobcat.reset()
-        bobcat.fastsync()
+try:
     bobcat.last_gap = bobcat.gap
+except:
+    print("Bobcat is not responding.  Rebooting via smart plug.")
+    switch.off()
+    time.sleep(10)
+    switch.on()
+    bobcat.last_gap = bobcat.gap
+
+count = 0
+while True:
+    try:
+        print([getattr(bobcat, attr) for attr in ['timestamp','uptime','status','gap','miner_height','blockchain_height','ota_version','temp0','temp1','errors']])
+        if status == "Down":
+            if count == 3:
+                print("Miner has been down for 3-cycles.  Rebooting via API.")
+                bobcat.reboot()
+            else:
+                count += count
+        else:
+            count = 0
+            if bobcat.gap > 10 and bobcat.gap > bobcat.last_gap:
+                bobcat.reset()
+                bobcat.fastsync()
+            bobcat.last_gap = bobcat.gap
+    except:
+        print("Bobcat is not repsonding.  Rebooting via smart plug.")
+        switch.off()
+        time.sleep(10)
+        switch.on()
     time.sleep(sleep_mins*60)
