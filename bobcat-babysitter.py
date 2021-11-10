975efeb715d50eb8c2814ac1388780998f16b935
Randy C. Will <randall.will@gmail.com>
2021-11-10 07:52:32 -0800

    Tasmota support and fault management improvements

--- bobcat-babysitter.py
+++ bobcat-babysitter.py
@@ -1,42 +1,59 @@
 #!/usr/bin/python3 -u
 
 import time
-import pywemo
-from bobcat import Bobcat
+#import pywemo
+from pybobcat import Bobcat
+from pytasmota import Tasmota
 
 bobcat = Bobcat('your bobcat ip')
-switch = pywemo.discovery.device_from_description('http://your-wemo-ip:49153/setup.xml')
+switch = Tasmota('your tasmota switch ip',your_outlet_id)
+#switch = pywemo.discovery.device_from_description('http://your-wemo-ip:49153/setup.xml')
 
 sleep_mins = 5
 
-print("Entering loop")
+print("Setting up")
 try:
     bobcat.last_gap = bobcat.gap
-except:
-    print("Bobcat is not responding.  Rebooting via smart plug.")
+except Exception as e:
+    print(f"{e}  Rebooting via smart plug.")
     switch.off()
     time.sleep(10)
     switch.on()
     bobcat.last_gap = bobcat.gap
 
+print("Entering loop")
 count = 0
 while True:
     try:
-        print([getattr(bobcat, attr) for attr in ['timestamp','uptime','status','gap','miner_height','blockchain_height','ota_version','temp0','temp1','errors']])
-        if status == "Down":
+        print([
+            bobcat.timestamp,
+            bobcat.uptime,
+            bobcat.status,
+            bobcat.gap,
+            bobcat.miner_height,
+            bobcat.blockchain_height,
+            bobcat.ota_version,
+            bobcat.miner["Image"],
+            bobcat.temp0,
+            bobcat.temp1,
+            bobcat.errors
+        ])
+        if bobcat.status == "Down":
             if count == 3:
                 print("Miner has been down for 3-cycles.  Rebooting via API.")
                 bobcat.reboot()
             else:
                 count += count
+        elif bobcat.status == "Helium API Timeout":
+            print("Helium API is having issues -- nothing we can do.  Waiting it out.")
         else:
             count = 0
             if bobcat.gap > 10 and bobcat.gap > bobcat.last_gap:
                 bobcat.reset()
                 bobcat.fastsync()
             bobcat.last_gap = bobcat.gap
-    except:
-        print("Bobcat is not repsonding.  Rebooting via smart plug.")
+    except Exception as e:
+        print(f"{e}  Rebooting via smart plug.")
         switch.off()
         time.sleep(10)
         switch.on()
